8fa2684548f14bb52458e7d2ef1ffd04c67c55b7
Sahil <sahilsarin390@gmail.com>
2022-02-22 23:02:07 +0530

    1281st

---
+++ C++/1281-SubtractProduct&SumofDigits.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int subtractProductAndSum(int n)
+    {
+        int sum = 0, product = 1;
+        for (; n > 0; n /= 10)
+        {
+            cout << n % 10 << endl;
+            sum += n % 10;
+            product *= n % 10;
+        }
+        return product - sum;
+    }
+};
